9311993c75ffd92cc6ce74e70317324a6b82af7b
Izolentiy <nima.munkonov@gmail.com>
2024-12-03 16:12:19 +0700

    Save state. Clock configuration failed

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,14 +3,13 @@
 #include "logic.h"
 
 int main(void) {
-    // GPIO_Ini();
+    GPIO_Ini();
     RCC_Ini();
     ITR_Ini();
     SysTick_Ini();
 
     init_periods_and_ticks();
     while (1) {
-        if (1 == 1) {}
         // manage_leds();
     }
 }
